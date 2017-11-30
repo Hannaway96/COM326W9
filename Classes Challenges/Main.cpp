03b4cf2a0d02f54e4d6b99bfaf89c4369c902a89
Jack <hannaway-j5@ulster.ac.uk>
2017-11-30 15:14:26 +0000

    completed half the challenges

--- Classes Challenges/Main.cpp
+++ Classes Challenges/Main.cpp
@@ -9,12 +9,16 @@
 */
 
 #include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <vector>
 #include "Student.h"
 #include "Module.h"
 #include "Person.h"
 #include "Lecturer.h"
 
 using namespace std;
+void WriteStudentDetails(std::vector<Student>& data);
 
 int main() {
 
@@ -29,14 +33,58 @@ int main() {
 	//With virtual functions the version called is based on the type of the object, not the pointer
 
 	//Create a pointer of person class and point it at the student object
+	Module OOP ("Object Orientated Programming", "COM326", 100);
+	Module VP ("Visual Programming", "COM321", 100);
+
 	Student stu1("Jeni Watt", "B004568656", "BSc Computing", 1);
-	
+	Student stu2("Jack Hannaway", "B00708431", "BSc Computing", 2);
+	Student stu3("John Doe", "B004568234", "BSc Computing", 4);
+
+	stu1.AddModule(OOP);
+	stu1.AddModule(VP);
+
+	stu2.AddModule(OOP);
+	stu2.AddModule(VP);
+
+	stu3.AddModule(OOP);
+	stu3.AddModule(VP);
+
 	pPtr = &stu1;
 	cout << pPtr->ToString();
 
 	//Always set pointers to nullptr when you are finished using them
 	pPtr = nullptr;
 
+	vector<Student> Student;
+	Student.push_back(stu1);
+	Student.push_back(stu2);
+	Student.push_back(stu3);
+
+	WriteStudentDetails(Student);
+
 	return 0;
 }
 
+
+void WriteStudentDetails(std::vector<Student>& data)
+{
+	ofstream outFile{};
+	outFile.open("StudentData.txt", ios::app);
+
+	if (!outFile) {
+		cerr << "File could not be opened" << endl;
+		exit(EXIT_FAILURE);
+	}
+
+	for (int i = 0; i < data.size(); i++) {
+	
+		outFile << " Reg: " << data[i].GetRegistrationID() << " Name: " << data[i].GetName() << " Course: " << data[i].GetCourse() << " Year of Study: " << data[i].GetYearofStudy() << endl;
+	}
+
+	outFile.close();
+}
+
+vector<Student> ReadStudentDetails(string dataFile)
+{
+	
+}
